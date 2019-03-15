3e31b6ac77d2169713d1ec4b3e6d3347c6bfa8c4
Ian Main <imain@redhat.com>
2019-03-15 10:12:15 -0700

    Need to debug.. grrr..

--- runironic.sh
+++ runironic.sh
@@ -11,6 +11,6 @@ fi
 /usr/bin/python2 /usr/bin/ironic-api > /var/log/ironic-api.out 2>&1 &
 /usr/sbin/httpd > /var/log/httpd.out 2>&1 &
 /usr/sbin/dnsmasq -d -q -C /etc/dnsmasq.conf > /var/log/dnsmasq.out 2>&1 &
-/bin/runhealthcheck &
+#/bin/runhealthcheck &
 sleep infinity
 
